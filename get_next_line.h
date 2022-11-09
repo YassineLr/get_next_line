5eaba8fc58fe3595a7934534cc9d7ea548957eaf
ylarhris <y.larhrissi.2001@gmail.com>
2022-11-09 03:25:27 +0100

    first try

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylarhris <ylarhris@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 18:44:13 by ylarhris          #+#    #+#             */
+/*   Updated: 2022/11/09 01:57:56 by ylarhris         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE
+# endif
+
+
+
+# endif
